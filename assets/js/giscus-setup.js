67ecdf78fd5543076641d77883c4208d56100d62
doub7e <41856955+doub7e@users.noreply.github.com>
2025-10-03 16:30:16 +0000

    Deploying to gh-pages from @ doub7e/doub7e.github.io@7c75347f6330f19336838f826e192b06fe3efc6c 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "doub7e/doub7e",
+    "data-repo": "doub7e/doub7e.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
